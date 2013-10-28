c3283c0142cb89f32cac4657d4cfd6f48b6cb720
nabihbarakat <barakat_nabih@hotmail.com>
2013-10-28 23:28:42 +0200

    .

--- DB.sql
+++ DB.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 18, 2013 at 11:34 AM
+-- Generation Time: Oct 28, 2013 at 11:26 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.3.21
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `student` (
   `Mobile` varchar(100) COLLATE utf8_bin NOT NULL,
   `gcmID` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `student`
@@ -41,7 +41,30 @@ CREATE TABLE IF NOT EXISTS `student` (
 
 INSERT INTO `student` (`Id`, `Name`, `Email`, `Mobile`, `gcmID`) VALUES
 (1, 'nabih barakat', 'barakat_nabih@hotmail.com', '009613144127', ''),
-(2, 'ali', 'a@nabihbarakat.com', '0000000', '');
+(2, 'elie', '0000000', '123123123123123', ' '),
+(3, 'student5', '34333dfsdf', '123321123321', ' '),
+(4, 'student7', 'email@student6.comcmm', '0001112223923', ' '),
+(5, 'student9', 'ss@ss.sss', '213402934', ' ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin123');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
